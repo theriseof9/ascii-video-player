c72c6dc55fd986c673f5fe1b55b7cc5980ac3ca4
CryptoAlgo-Dev <cryptoalgro@gmail.com>
2021-06-01 12:29:21 +0800

    Added density array and libraries to find terminal size

--- c++/video-to-ascii/video-to-ascii/main.cpp
+++ c++/video-to-ascii/video-to-ascii/main.cpp
@@ -8,45 +8,53 @@
 #include "opencv2/opencv.hpp"
 #include <iostream>
 
+// Finding terminal size
+#include <sys/ioctl.h> //ioctl() and TIOCGWINSZ
+#include <unistd.h> // for STDOUT_FILENO
+
 using namespace std;
 using namespace cv;
 
-int main(){
+// ====== //
+// Globals
+const string DENSITY[] = {
+    " ", " ", ".", ":", "!", "+", "*", "e", "$", "@", "8",
+    ".", "*", "e", "s", "◍",
+    "░", "▒", "▓", "█"
+};
 
-  // Create a VideoCapture object and open the input file
-  // If the input is the web camera, pass 0 instead of the video file name
-  VideoCapture cap("chaplin.mp4");
-   
-  // Check if camera opened successfully
-  if(!cap.isOpened()){
-    cout << "Error opening video stream or file" << endl;
-    return -1;
-  }
-    
-  while(1){
+int main() {
+    // Create a VideoCapture object and open the input file
+    // If the input is the web camera, pass 0 instead of the video file name
+    VideoCapture cap("/Users/vinkwok/Documents/Xcode Projects/ascii-video-player/c++/video-to-ascii/video-to-ascii/test-vid.mp4");
 
-    Mat frame;
-    // Capture frame-by-frame
-    cap >> frame;
- 
-    // If the frame is empty, break immediately
-    if (frame.empty())
-      break;
+    // Check if camera opened successfully
+    if (!cap.isOpened()) {
+        cout << "Error opening video stream or file" << endl;
+        return -1;
+    }
 
-    // Display the resulting frame
-    imshow( "Frame", frame );
+    while (1) {
+        Mat frame;
+        // Capture frame-by-frame
+        cap >> frame;
 
-    // Press  ESC on keyboard to exit
-    char c=(char)waitKey(25);
-    if(c==27)
-      break;
-  }
- 
-  // When everything done, release the video capture object
-  cap.release();
+        // If the frame is empty, break immediately
+        if (frame.empty()) break;
 
-  // Closes all the frames
-  destroyAllWindows();
-    
-  return 0;
+        // Display the resulting frame
+        imshow("Frame", frame);
+
+        // Press  ESC on keyboard to exit
+        char c = (char) waitKey(25);
+        if (c == 27) break;
+    }
+
+    // When everything done, release the video capture object
+    cap.release();
+
+    // Closes all the frames
+    destroyAllWindows();
+
+    return 0;
 }
