0ec0c799e9fa7f7e525561df61d34e2268186fb8
CryptoAlgo-Dev <cryptoalgro@gmail.com>
2021-06-01 15:10:13 +0800

    Added greyscale rendering

--- c++/video-to-ascii/video-to-ascii/main.cpp
+++ c++/video-to-ascii/video-to-ascii/main.cpp
@@ -28,11 +28,16 @@ const string DENSITY[] = {
 struct winsize termSize;
 
 int main() {
+    // Fast IO speed
+    cout.tie(0);
+    ios_base::sync_with_stdio(0);
+    
     // Create a VideoCapture object and open the input file
     // If the input is the web camera, pass 0 instead of the video file name
     // Find home dir (on unix only)
     string HOME(getenv("HOME"));
     string vidPath("/video.mp4");
+    // HOME + vidPath
     VideoCapture cap(HOME + vidPath);
 
     // Check if camera opened successfully
@@ -54,11 +59,29 @@ int main() {
         
         // Resize frame to the size of the terminal
         resize(frame, frame, Size(termSize.ws_col, termSize.ws_row), 0, 0, INTER_AREA);
+        
+        uint8_t* pixelPtr = (uint8_t*)frame.data;
+        int cn = frame.channels();
+        Scalar_<uint8_t> bgrPixel;
+
+        for(int i = 0; i < frame.rows; i++) {
+            for(int j = 0; j < frame.cols; j++) {
+                bgrPixel.val[2] = pixelPtr[i * frame.cols * cn + j * cn + 0]; // B
+                bgrPixel.val[1] = pixelPtr[i * frame.cols * cn + j * cn + 1]; // G
+                bgrPixel.val[0] = pixelPtr[i * frame.cols * cn + j * cn + 2]; // R
+
+                // do something with RGB values...
+                const uint8_t intensity = (bgrPixel[0] + bgrPixel[1] + bgrPixel[2]) / 60;
+                cout << DENSITY[intensity];
+            }
+            cout << endl;
+        }
+        cout << flush;
 
         // Display the resulting frame
         imshow("Output", frame);
 
-        // Press  ESC on keyboard to exit
+        // Press ESC on keyboard to exit
         char c = (char) waitKey(25);
         if (c == 27) break;
     }
