30522b25ee9cd168daf7fb68516fe3232cdc84ba
Wang Zerui <h2010155@nushigh.edu.sg>
2021-06-01 09:24:37 +0800

    added xcode proj

---
+++ c++/video-to-ascii/video-to-ascii/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  video-to-ascii
+//
+//  Created by Wang Zerui on 1/6/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
