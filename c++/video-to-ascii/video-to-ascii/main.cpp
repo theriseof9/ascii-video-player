b6ed92969cc3b27aa92dff08a5ec15b41edc115a
Wang Zerui <h2010155@nushigh.edu.sg>
2021-06-01 10:41:15 +0800

    changed xcode settings

--- c++/video-to-ascii/video-to-ascii/main.cpp
+++ c++/video-to-ascii/video-to-ascii/main.cpp
@@ -5,10 +5,48 @@
 //  Created by Wang Zerui on 1/6/21.
 //
 
+#include "opencv2/opencv.hpp"
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+using namespace cv;
+
+int main(){
+
+  // Create a VideoCapture object and open the input file
+  // If the input is the web camera, pass 0 instead of the video file name
+  VideoCapture cap("chaplin.mp4");
+   
+  // Check if camera opened successfully
+  if(!cap.isOpened()){
+    cout << "Error opening video stream or file" << endl;
+    return -1;
+  }
+    
+  while(1){
+
+    Mat frame;
+    // Capture frame-by-frame
+    cap >> frame;
+ 
+    // If the frame is empty, break immediately
+    if (frame.empty())
+      break;
+
+    // Display the resulting frame
+    imshow( "Frame", frame );
+
+    // Press  ESC on keyboard to exit
+    char c=(char)waitKey(25);
+    if(c==27)
+      break;
+  }
+ 
+  // When everything done, release the video capture object
+  cap.release();
+
+  // Closes all the frames
+  destroyAllWindows();
+    
+  return 0;
 }
