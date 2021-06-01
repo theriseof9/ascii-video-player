a69c0cdcb481e42c6b25262345f71e437b9f922b
CryptoAlgo-Dev <cryptoalgro@gmail.com>
2021-06-01 16:00:17 +0800

    Added colors

---
+++ c++/video-to-ascii/video-to-ascii/colorUtil.hpp
@@ -0,0 +1,13 @@
+//
+//  colorUtil.hpp
+//  video-to-ascii
+//
+//  Created by Vincent Kwok on 1/6/21.
+//
+
+#ifndef colorUtil_hpp
+#define colorUtil_hpp
+
+int getColorId(int r1, int g1, int b_);
+
+#endif /* colorUtil_hpp */
